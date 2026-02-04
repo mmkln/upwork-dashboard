a817d12d4639cd407e42e5a2e1fcea18991fdebd
Michael Mikulin <m.mikulin@outlook.com>
2026-02-04 15:32:54 +0100

    feat(ui): add input disabled state styles

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -6,7 +6,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className = "", ...props }, ref) => (
     <input
       ref={ref}
-      className={`block w-full rounded-[10px] border border-[#EFF0F0] h-10 px-4 text-sm text-[#141414] placeholder:text-sm placeholder:font-normal placeholder:text-[#C8C8C8] focus:border-[#1823F0] focus:outline-none focus:ring-2 focus:ring-[#C4C4FD] ${className}`}
+      className={`block w-full rounded-[10px] border border-[#EFF0F0] h-10 px-4 text-sm text-[#141414] placeholder:text-sm placeholder:font-normal placeholder:text-[#C8C8C8] focus:border-[#1823F0] focus:outline-none focus:ring-2 focus:ring-[#C4C4FD] disabled:bg-[#EFF0F0] disabled:cursor-not-allowed ${className}`}
       {...props}
     />
   ),
